39afa40dc0c7f8d4d05eea574069f60a24312773
Сергей Корбут <siarheikorbut@gmail.com>
2020-10-29 22:41:38 +0300

    Java Syntax (level 9/tasks 5) completed.

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0905/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task09.task0905;
+
+/* 
+Там, в синих глубинах стек-трейса…
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int deep = getStackTraceDepth();
+    }
+
+    public static int getStackTraceDepth() {
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        System.out.println(stackTraceElements.length);
+        return stackTraceElements.length;//напишите тут ваш код
+    }
+}
