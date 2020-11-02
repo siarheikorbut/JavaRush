85a2f9d5f92c01550226a2c6a2b339997aae4339
siarheikorbut <siarheikorbut@gmail.com>
2020-11-02 16:47:42 +0300

    Java Syntax (level 9/task 8) completed.

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0908/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task09.task0908;
+
+/* 
+Исключение при работе со строками
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try {
+            String s = null;
+            String m = s.toLowerCase();//напишите тут ваш код
+        } catch (NullPointerException e){
+            System.out.println("NullPointerException: переменная s = null!");
+        }//напишите тут ваш код
+    }
+}
