9264f5a94055653bb98e3becb0c37f3fbe5c759c
siarheikorbut <siarheikorbut@gmail.com>
2020-11-02 19:27:52 +0300

    Java Syntax (level 9/task 13) completed.

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0913/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task09.task0913;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+
+/* 
+Исключения. Просто исключения.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        try {
+            method1();//напишите тут ваш код
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException");
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException");
+        }//напишите тут ваш код
+    }
+
+    public static void method1() throws NullPointerException, ArithmeticException, FileNotFoundException, URISyntaxException {
+        int i = (int) (Math.random() * 4);
+        if (i == 0) {
+            throw new NullPointerException();
+        } else if (i == 1) {
+            throw new ArithmeticException();
+        } else if (i == 2) {
+            throw new FileNotFoundException();
+        } else if (i == 3) {
+            throw new URISyntaxException("", "");
+        }
+    }
+}
