6da3d4f30440e2fb5dedf3e148ed169f3fff1656
siarheikorbut <siarheikorbut@gmail.com>
2020-11-10 18:36:44 +0300

    Java Syntax (level 7/task 20) completed.

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0720/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task07.task0720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/* 
+Перестановочка подоспела
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> list = new ArrayList<>();
+
+        int n = Integer.parseInt(reader.readLine());
+        int m = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < n; i++) {
+            list.add(reader.readLine());
+        }
+
+        Collections.rotate(list, -m);
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
