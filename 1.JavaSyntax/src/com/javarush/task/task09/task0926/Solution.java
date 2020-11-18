cabdd9a6a7a6d440dbc3de44ea6e8d086d86501a
siarheikorbut <siarheikorbut@gmail.com>
2020-11-18 17:43:07 +0300

    Java Syntax (level 9/task 26) completed.

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0926/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task09.task0926;
+
+import java.util.ArrayList;
+
+/* 
+Список из массивов чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<int[]> list = createList();
+        printList(list);
+    }
+
+    public static ArrayList<int[]> createList() {
+        ArrayList<int[]> array = new ArrayList<>();
+        array.add(new int[5]);
+        array.add(new int[2]);
+        array.add(new int[4]);
+        array.add(new int[7]);
+        array.add(new int[0]);
+        return array;//напишите тут ваш код
+    }
+
+    public static void printList(ArrayList<int[]> list) {
+        for (int[] array : list) {
+            for (int x : array) {
+                System.out.println(x);
+            }
+        }
+    }
+}
