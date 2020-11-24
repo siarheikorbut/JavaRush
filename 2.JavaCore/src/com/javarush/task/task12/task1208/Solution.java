31b0e597d38518bfde1852b910c37386616275c1
siarheikorbut <siarheikorbut@gmail.com>
2020-11-24 18:18:19 +0300

    Java Core (level 2/task 8) completed.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(byte number) {
+
+    }
+
+    public static void print(short number) {
+
+    }
+
+    public static void print(int number) {
+
+    }
+
+    public static void print(long number) {
+
+    }
+
+    public static void print(float number) {
+
+    }//Напишите тут ваши методы
+}
