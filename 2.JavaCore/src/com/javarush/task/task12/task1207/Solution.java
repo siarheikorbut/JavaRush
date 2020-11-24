6532a60d846f853168fbab5b013fdb52015228b4
siarheikorbut <siarheikorbut@gmail.com>
2020-11-24 18:12:41 +0300

    Java Core (level 2/task 7) completed.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int x = (int) (Math.random() * 11);
+        Integer y = (int) (Math.random() * 11);
+        print(x);
+        print(y);
+    }
+
+    public static void print(int number) {
+
+    }
+
+    public static void print(Integer number) {
+
+    }//Напишите тут ваши методы
+}
