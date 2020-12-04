5ed59cff1e14e60fb92b97eed894a6d88acbfd39
siarheikorbut <siarheikorbut@gmail.com>
2020-12-05 00:23:58 +0300

    Java Core (level 3/task 19) completed.

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task13.task1319;
+
+import java.io.*;
+
+/* 
+Писатель в файл с консоли
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String destinationFileName = reader.readLine();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(destinationFileName));
+
+        StringBuilder builder = new StringBuilder();
+
+        String s = "";
+        while (!s.equals("exit")) {
+            s = reader.readLine();
+            builder.append(s).append("\n");
+        }
+
+        writer.write(builder.toString());
+        writer.close();
+    }
+}
