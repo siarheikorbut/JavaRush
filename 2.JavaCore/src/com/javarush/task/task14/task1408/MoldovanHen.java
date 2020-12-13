944148e5db3e62e05dc58bc004bd5e62621f9626
siarheikorbut <siarheikorbut@gmail.com>
2020-12-13 17:09:48 +0300

    Java Core (level 4/task 8) completed.

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1408/MoldovanHen.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task14.task1408;
+
+public class MoldovanHen extends Hen {
+    @Override
+    int getCountOfEggsPerMonth() {
+        return 28;
+    }
+
+    @Override
+    String getDescription() {
+        return super.getDescription() +
+                " Моя страна - " +
+                Country.MOLDOVA +
+                ". Я несу " +
+                getCountOfEggsPerMonth() +
+                " яиц в месяц."
+                ;
+    }
+}
