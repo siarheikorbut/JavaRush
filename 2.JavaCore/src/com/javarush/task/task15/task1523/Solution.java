5a4715a94ecc3c55496910c94285e741f561f179
siarheikorbut <siarheikorbut@gmail.com>
2020-12-18 18:32:06 +0300

    Java Core (level 5/task 23) completed.

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public Solution(int i) {
+    }
+
+    protected Solution(boolean b) {
+
+    }
+
+    Solution(double d) {
+
+    }
+
+    private Solution(String s) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
