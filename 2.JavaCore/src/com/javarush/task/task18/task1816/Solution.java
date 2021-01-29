b198ec7139d97ed61201c135e0f150cf5f06e83b
siarheikorbut <siarheikorbut@gmail.com>
2021-01-29 18:28:27 +0300

    Java Core (level 8/task 16) completed.

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task18.task1816;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/* 
+Английские буквы
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (FileReader fileReader = new FileReader(args[0])) {
+            int count = 0;
+            while (fileReader.ready()) {
+                char readChar = (char) fileReader.read();
+                String s = "" + readChar;
+                if (s.matches("[a-zA-Z]")) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
